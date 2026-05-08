18b5b7625e438b64b196f1303640500ccd1d47cf
Victor J Calderon <vicjocaso@gmail.com>
2026-05-08 13:57:00 -0400

    Fix broken aliases referencing removed tools

--- install/desktop/set-gnome-extensions.sh
+++ install/desktop/set-gnome-extensions.sh
@@ -3,9 +3,10 @@
 sudo apt install -y gnome-shell-extension-manager gir1.2-gtop-2.0 gir1.2-clutter-1.0
 pipx install gnome-extensions-cli --system-site-packages
 
-# Turn off default Ubuntu extensions (keep ubuntu-dock for Super key app grid)
+# Turn off default Ubuntu extensions
 gnome-extensions disable tiling-assistant@ubuntu.com
 gnome-extensions disable ubuntu-appindicators@ubuntu.com
+gnome-extensions disable ubuntu-dock@ubuntu.com
 gnome-extensions disable ding@rastersoft.com
 
 # Pause to assure user is ready to accept confirmations
