43bde3ce3f301680bb139c7b867ddd26929073c6
Victor J Calderon <vicjocaso@gmail.com>
2026-06-11 22:47:55 -0400

    fix: remove gnome-software-plugin-flatpak (not available on Fedora)

--- fedora/install/desktop/a-flatpak.sh
+++ fedora/install/desktop/a-flatpak.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Flatpak is pre-installed on Fedora; just ensure Flathub is added
-sudo dnf install -y flatpak gnome-software-plugin-flatpak
+sudo dnf install -y flatpak
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
